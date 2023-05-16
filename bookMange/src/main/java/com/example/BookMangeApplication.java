43f4e5dc53e03efa67d206da6a60148096756598
niuhanjie <nullkey.@163.com>
2023-05-16 17:30:20 +0800

    first commit

---
+++ bookMange/src/main/java/com/example/BookMangeApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMangeApplication.class, args);
+    }
+
+}
---
+++ bookMange/src/main/java/com/example/controller/utils/ResultCode.java
@@ -0,0 +1,6 @@
+package com.example.controller.utils;
+
+public interface ResultCode {
+    public static Integer SUCCESS = 2000;  //成功码
+    public static Integer ERROR = 2001;  //失败码
+}
